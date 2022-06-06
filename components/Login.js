5af835198b227df1cfa4e8ff1f16db9300453745
AllanOliveira22 <allanmoreira28@gmail.com>
2022-06-05 23:20:36 -0300

    Login Screen base

--- components/Login.js
+++ components/Login.js
@@ -1,39 +1,97 @@
 import react, {useState} from "react";
-import { StyleSheet, View } from "react-native"; 
-import { Input, Text, Button } from "react-native-elements";
+import { StyleSheet, View, KeyboardAvoidingView, TouchableOpacity, TextInput } from "react-native"; 
+import { Input, Text, Button, Image } from "react-native-elements";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 export default function Login( {navigation} ){
-    const [login, setLogin] = useState(null);
-    const [password, setPassword] = useState(null);
-    return (
-        <View style={styles.container}>
-            <View style={styles.innercontainer}>
-                <Input style={styles.input} placeholder="Login" onChangeText={value=>setLogin(value)} />
-                <Input style={styles.input} placeholder="Senha" onChangeText={value=>setPassword(value)} secureTextEntry={true} />
-                <Button style={{margin: 20}} title="Login" />
-            </View>
-        </View>
-    );
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Image
+          source={require('../src/image/Logo.png')}
+        />
+      </View>
+
+      <View style={styles.container}>
+        <TextInput style={styles.input}
+        placeholder="Email"
+        autoCorrect={false}
+        onChangeText={()=> {}}
+        />
+
+      <TextInput style={styles.input}
+        placeholder="Senha"
+        autoCorrect={false}
+        onChangeText={()=> {}}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar Conta</Text>
+        </TouchableOpacity>
+
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#eee'
-    },
-    innercontainer:{
-        backgroundColor: '#fff',
-        padding: 50,
-        maxWidth: 600,
-    },
-    input:{
-        borderBottom: 'solid 1px #46d',
-        padding: 10,
-        margin: 5,
+  background:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF'
+  },
 
-    }
-})
+  containerLogo:{
+    flex:1,
+    justifyContent:'center'
+  },
+
+  container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 100
+  },
+
+  input:{
+    backgroundColor: 'lightgray',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize:17,
+    borderRadius:7,
+    borderStyle: 'solid',
+    borderColor: '#191919',
+    padding: 8
+  },
+
+  btnSubmit:{
+    backgroundColor:'#35AAFF',
+    width: '90%',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+
+  submitText:{
+    color:'#FFF',
+    fontSize: 18
+  },
+
+  btnRegister:{
+    marginTop: 15
+  },
+
+  registerText:{
+    color: '#191919'
+  }
+
+});
